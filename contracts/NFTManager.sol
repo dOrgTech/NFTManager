bdc3d19ee9359362526445cab6419c30c35a59c0
tspoff@gmail.com <tspoff@gmail.com>
2020-05-19 14:43:43 -0500

    Fix imports

--- contracts/NFTManager.sol
+++ contracts/NFTManager.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.6.8;
 
-import IERC721 from "@openzeppelin/contracts-ethereum-package/token/ERC721/IERC721.sol"
-import Ownable from "@openzeppelin/contracts-ethereum-package/access/Ownable.sol"
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 
-contract NFTManager is Ownable {
+contract NFTManager is OwnableUpgradeSafe {
     // @notice Transfer specified NFT to 
     function sendNFT(IERC721 nftContract, uint tokenId, address recipient) external {
         nftContract.transferFrom(address(this), recipient, tokenId);
